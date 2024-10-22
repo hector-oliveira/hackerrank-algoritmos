0cf5f158734d1814b6a3913912e1fceca73695d7
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-22 14:24:56 -0300

    test: Bill Division

--- algorithms/21-BillDivision.ts
+++ algorithms/21-BillDivision.ts
@@ -10,3 +10,12 @@ function bonAppetit(bill: number[], k: number, b: number): void {
   const billAnna = (bill.reduce((a, b) => a + b, 0) - bill[k]) / 2;
   billAnna === b ? console.log("Bon Appetit") : console.log(b - billAnna);
 }
+
+// test 0
+bonAppetit([2, 4, 6], 2, 3); // 'Bon Appetit'
+// test 1
+bonAppetit([3, 10, 2, 9], 1, 7); // 'Bon Appetit'
+// test 2
+bonAppetit([5, 7, 6, 3, 3], 2, 12); // 3
+// test 3
+bonAppetit([2, 2, 3, 4, 5], 0, 8); // 1
